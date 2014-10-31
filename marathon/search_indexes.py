52f30a7124936565d531e0627747ed906e7267a9
Raphaël Velt <raphael@velt.info>
2014-10-31 11:09:20 +0000

    search index update

--- marathon/search_indexes.py
+++ marathon/search_indexes.py
@@ -3,17 +3,14 @@ from marathon.models import Video
 
 class VideoIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
+    event = indexes.CharField(model_attr="event__name")
     start_time = indexes.DateTimeField(model_attr="start_time")
     end_time = indexes.DateTimeField(model_attr="end_time")
     distances = indexes.MultiValueField()
-    runners = indexes.MultiValueField()
     thumbnail = indexes.CharField(model_attr="thumbnail")
     
     def prepare_distances(self, obj):
         return [d.reference_point.distance for d in obj.videodistance_set.all()]
-    
-    def prepare_runners(self, obj):
-        return [d.label for d in obj.runnertags.all()]
     
     def get_model(self):
         return Video
